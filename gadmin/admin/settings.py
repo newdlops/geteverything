b8e01ea4f74593a8644ffe0f6c4bf220e3ef743b
keisp <kei@shoeprize.com>
2025-05-11 13:06:17 +0900

    action

--- gadmin/admin/settings.py
+++ gadmin/admin/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
