70abdbb830b712a27bde8ad56a17e4cf9d81e20e
newdlops <newdlops@gmail.com>
2025-06-29 18:21:31 +0900

    엑셀 다운로드

--- gadmin/deals/admin.py
+++ gadmin/deals/admin.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import FieldDoesNotExist
 from django.http import StreamingHttpResponse
 from django.contrib import admin
 import xlsx_streaming
+import itertools
 from deals.models import Deal
 from django.utils.html import format_html
 from .export_filelds_action_form import ExportFieldsActionForm
@@ -19,8 +21,8 @@ class DealAdmin(AdminActionFormsMixin, admin.ModelAdmin):
         'create_at',
         'recommend_count',
         'view_count',
-        'origin_link',
-        'shop_url',
+        'origin_url',
+        'shop_url_1',
         'article_id', #
         'shop_name',
         'thumbnail',
@@ -44,9 +46,30 @@ class DealAdmin(AdminActionFormsMixin, admin.ModelAdmin):
         쿼리셋을 xlsx_streaming으로 스트림 생성 후 StreamingHttpResponse로 반환
         """
         # values_list에 내보낼 컬럼 지정
-        qs_values = queryset.values_list('community_name', 'category', 'subject', 'write_at', 'create_at', 'recommend_count', 'view_count')
+
+        model = self.model
+        selected = form['fields_to_export']
+        header_labels = []
+        for field_name in selected:
+            try:
+                field = model._meta.get_field(field_name)
+                label = str(field.verbose_name)
+            except FieldDoesNotExist:
+                # list_display에 정의된 admin 메서드나 속성일 때
+                attr = getattr(self, field_name, None)
+                label = getattr(attr, 'short_description', None) \
+                        or field_name.replace('_', ' ').capitalize()
+            header_labels.append(label)
+
+
+        header = tuple(header_labels)
+        data_iter = itertools.chain(
+            [header],
+            queryset.values_list(*selected).iterator(chunk_size=100)
+        )
+
         stream = xlsx_streaming.stream_queryset_as_xlsx(
-            qs_values,
+            data_iter,
             batch_size=100
         )
         response = StreamingHttpResponse(
@@ -60,10 +83,10 @@ class DealAdmin(AdminActionFormsMixin, admin.ModelAdmin):
     # action_form = ExportFieldsActionForm
     actions = ['action_stream_xlsx']
 
-    def origin_link(self, obj):
+    def origin_url(self, obj):
         return format_html('<a href="{}" target="_blank" rel="noopener noreferrer">{}</a>', obj.origin_url, obj.origin_url)
-    origin_link.short_description = '원글 링크'
+    origin_url.short_description = '원글 링크'
 
-    def shop_url(self, obj):
+    def shop_url_1(self, obj):
         return format_html('<a href="{}" target="_blank" rel="noopener noreferrer">{}</a>', obj.shop_url_1, obj.shop_url_1)
-    shop_url.short_description = '쇼핑몰 링크'
+    shop_url_1.short_description = '쇼핑몰 링크'
--- gadmin/deals/export_filelds_action_form.py
+++ gadmin/deals/export_filelds_action_form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import FieldDoesNotExist
 from django_admin_action_forms import AdminActionForm
 
 class ExportFieldsActionForm(AdminActionForm):
@@ -11,6 +12,15 @@ class ExportFieldsActionForm(AdminActionForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.modeladmin, self.request, self.queryset 사용 가능
-        choices = [(f, f) for f in self.modeladmin.list_display]
+        model = self.modeladmin.model
+        choices = []
+        for field_name in self.modeladmin.list_display:
+            try:
+                field = model._meta.get_field(field_name)
+                verbose = getattr(field, 'verbose_name', None)
+                label = str(verbose) if verbose else field_name.replace('_', ' ').capitalize()
+            except FieldDoesNotExist:
+                label = field_name.replace('_', ' ').capitalize()
+            choices.append((field_name, label))
         self.fields['fields_to_export'].choices = choices
-        self.fields['fields_to_export'].initial = [f for f, _ in choices]
+        self.fields['fields_to_export'].initial = [name for name, _ in choices]
