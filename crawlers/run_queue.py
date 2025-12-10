8ffbc895c9c41e5f2a0a32bb1d2f7b0aa3664c1a
newdlops <newdlops@gmail.com>
2025-12-11 03:30:13 +0900

    deploy

--- crawlers/run_queue.py
+++ crawlers/run_queue.py
@@ -56,8 +56,11 @@ async def producer():
     예: 각 사이트를 30분마다 한 번씩 돌리는 스케줄.
     """
     sites = [
-        # "ppomppu",
+        "ppomppu",
         "fmkorea",
+        "eomisae",
+        "coolnjoy",
+        "arca",
     ]
 
     while True:
