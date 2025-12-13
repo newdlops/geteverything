9beb7e3f49a1ebd5a42579eb917d76a789a44272
newdlops <newdlops@gmail.com>
2025-12-13 16:05:33 +0900

    summernote 관련 빌드 실패 수정

--- gadmin/summernote_appconfig.py
+++ gadmin/summernote_appconfig.py
@@ -1,5 +1,6 @@
-from django.apps import AppConfig
+from django_summernote.apps import DjangoSummernoteConfig
 
-class DjangoSummernotePinnedAutoFieldConfig(AppConfig):
+
+class DjangoSummernotePinnedAutoFieldConfig(DjangoSummernoteConfig):
     name = "django_summernote"
     default_auto_field = "django.db.models.AutoField"
